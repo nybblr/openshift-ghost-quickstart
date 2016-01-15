c356104a8285d870fe05ff2c61d85b6d660295ab
Jonathan Martin <me@nybblr.com>
2016-01-15 17:04:20 -0500

    Pull home URL into env var.

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
   // When running Ghost in the wild, use the production environment
   // Configure your URL and mail settings here
   production: {
-    url: 'https://'+'blog.yellowscale.com',
+    url: process.env.HOME_URL,
     forceAdminSSL: true,
     mail: {},
     server: {
